3520616dac71434ba6d35df6027e5d5a3ddd0c50
ArseniiTi <tiurin@sheridancollege.ca>
2019-06-13 22:13:57 -0400

    HW 1.7

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -11,17 +11,13 @@ import java.io.IOException;
 import static org.slf4j.LoggerFactory.getLogger;
 
 public class UserServlet extends HttpServlet {
+    private static final Logger log = getLogger(UserServlet.class);
 
-    private static final Logger log = getLogger(UserServlet.class); //you can create shortcut for this line(by my template)
-
-
-    //servlet saving info to the file(makes log)
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.debug("redirect to users");
 
 //        request.getRequestDispatcher("/users.jsp").forward(request, response);
-        response.sendRedirect("users.jsp"); //make redirect to this page
+        response.sendRedirect("users.jsp");
     }
 }
-
