e6c279df373d7a1d1b49eee1b3538046c4463217
ArseniiTi <tiurin@sheridancollege.ca>
2019-06-27 23:59:23 -0400

    3.6

--- src/test/java/ru/javawebinar/topjava/SpringMain.java
+++ src/test/java/ru/javawebinar/topjava/SpringMain.java
@@ -17,27 +17,27 @@ import java.util.List;
 
 public class SpringMain {
     public static void main(String[] args) {
-//        ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml"); //creates application context spring, which is described in xml, which is located in the classpath
-//        System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
+        ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml"); //creates application context spring, which is described in xml, which is located in the classpath
+        System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
+
+//        UserRepository userRepository = (UserRepository) appCtx.getBean("inmemoryUserRepository"); //can take any bins from context
+        UserRepository userRepository = appCtx.getBean(UserRepository.class); //can take any bins from context(take access from java EE toward spring)
+        userRepository.getAll();
+        appCtx.close(); //close all resources which been used during the work + all maps in the context spring will be deleted
+
+
+//        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml", "spring/inmemory.xml")) {
+//            System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
+//            AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
+//            adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ROLE_ADMIN));
+//            System.out.println();
 //
-////        UserRepository userRepository = (UserRepository) appCtx.getBean("inmemoryUserRepository"); //can take any bins from context
-//        UserRepository userRepository = appCtx.getBean(UserRepository.class); //can take any bins from context(take access from java EE toward spring)
-//        userRepository.getAll();
-//        appCtx.close(); //close all resources which been used during the work + all maps in the context spring will be deleted
-
-
-        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml", "spring/inmemory.xml")) {
-            System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
-            AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
-            adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ROLE_ADMIN));
-            System.out.println();
-
-            MealRestController mealController = appCtx.getBean(MealRestController.class);
-            List<MealTo> filteredMealsWithExcess =
-                    mealController.getBetween(
-                            LocalDate.of(2015, Month.MAY, 30), LocalTime.of(7, 0),
-                            LocalDate.of(2015, Month.MAY, 31), LocalTime.of(11, 0));
-            filteredMealsWithExcess.forEach(System.out::println);
-        }
+//            MealRestController mealController = appCtx.getBean(MealRestController.class);
+//            List<MealTo> filteredMealsWithExcess =
+//                    mealController.getBetween(
+//                            LocalDate.of(2015, Month.MAY, 30), LocalTime.of(7, 0),
+//                            LocalDate.of(2015, Month.MAY, 31), LocalTime.of(11, 0));
+//            filteredMealsWithExcess.forEach(System.out::println);
+//        }
     }
 }
